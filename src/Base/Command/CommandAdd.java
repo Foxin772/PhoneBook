e656cee12cc40016c55105f7db93b5032b67be87
Foxin772 <net-kda772@yandex.ru>
2016-04-12 16:25:20 +0700

    Book

--- src/Base/Command/CommandAdd.java
+++ src/Base/Command/CommandAdd.java
@@ -9,23 +9,25 @@ class CommandAdd implements Command  {
     @Override
     public void execute() throws Exception {
         String[] arguments;
-        ConsoleHalper.write("Введите значения записи в формате - Номер Имя");
+        ConsoleHalper.write("Введите значения записи в формате - Имя Номер");
         while (true) {
             try {
                 arguments = ConsoleHalper.getComand();
-                if (arguments[0].charAt(0) == '+')
-                Long.parseLong(arguments[0].substring(1));
+                if (arguments[1].charAt(0) == '+')
+                Long.parseLong(arguments[1].substring(1));
                 else
-                    Long.parseLong(arguments[0].substring(1));
-                if (PhoneBook.clients.containsKey(arguments[1])) {
-                    String bufer = PhoneBook.clients.get(arguments[1]) + ", " + arguments[0];
-                    PhoneBook.clients.put(arguments[1], bufer);
+                    Long.parseLong(arguments[1]);
+                if (PhoneBook.clients.containsKey(arguments[0])) {
+                    String bufer = PhoneBook.clients.get(arguments[0]) + ", " + arguments[1];
+                    PhoneBook.clients.put(arguments[0], bufer);
                 }else
-                PhoneBook.clients.put(arguments[1], arguments[0]);
+                    PhoneBook.clients.put(arguments[0], arguments[1]);
                 ConsoleHalper.write("Запись успешно добавлена");
                 ConsoleHalper.write("Добавить еще запись? (Да/Нет)");
                 if(ConsoleHalper.getAnswer().equalsIgnoreCase("нет"))
-                break;
+                    break;
+                else
+                    ConsoleHalper.write("Введите значения записи в формате - Имя Номер");
             } catch (CommandExit e) {
                 throw e;
             }catch (GoToMenuException e)
